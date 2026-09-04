3140b06ddbcc2279e1dd5571dbc1a991874386dd
SantosoParlindunganTogatorop <santosotogatorop@gmail.com>
2026-09-04 08:23:21 +0800

    add screenshot front.png

--- tugas_1/lib/main.dart
+++ tugas_1/lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() => runApp(const MyApp());
+
+final GoRouter _router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const ProfilePage()),
+    GoRoute(
+      path: '/detail',
+      builder: (context, state) => const HalamanDetail(),
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorSchemeSeed: Colors.blue, // ganti sesuai warna yang kamu mau
-        useMaterial3: true,
-      ),
-      home: ProfilePage(),
+      theme: ThemeData(colorSchemeSeed: Colors.blue, useMaterial3: true),
+      routerConfig: _router,
     );
   }
 }
@@ -39,35 +48,54 @@ class ProfilePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Foto profil
             const CircleAvatar(
               radius: 60,
               backgroundImage: AssetImage('assets/images/profile.png'),
             ),
             const SizedBox(height: 20),
-
-            // Nama
             const Text(
               'Santoso Parlindungan Togatorop',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
-
-            // NIM
             const Text(
               'NIM: 2109106052',
               style: TextStyle(fontSize: 16, color: Colors.grey),
             ),
             const SizedBox(height: 8),
-
             const Text(
               'Samarinda, Kalimantan Timur',
               style: TextStyle(fontSize: 16, color: Colors.grey),
             ),
             const SizedBox(height: 30),
-
-            ElevatedButton(onPressed: () {}, child: const Text('Jelajahi')),
+            ElevatedButton(
+              onPressed: () => context.push('/detail'),
+              child: const Text('Jelajahi'),
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class HalamanDetail extends StatelessWidget {
+  const HalamanDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Detail')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            if (context.canPop()) {
+              context.pop();
+            } else {
+              context.go('/');
+            }
+          },
+          child: const Text('Kembali ⬅️'),
         ),
       ),
     );
